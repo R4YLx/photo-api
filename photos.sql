ab8287044018e13156adbdc513fe10180cc87a6d
Raymond Lam <raymond.m.lam@gmail.com>
2022-02-28 17:37:09 +0100

    added profile validation rules

--- photos.sql
+++ photos.sql
@@ -3,6 +3,7 @@ CREATE TABLE users (
     first_name VARCHAR(250) NOT NULL,
     last_name VARCHAR(250) NOT NULL,
     email VARCHAR(250) NOT NULL,
+    password VARCHAR(250) NOT NULL,
     PRIMARY KEY(id)
 );
 
@@ -19,7 +20,9 @@ CREATE TABLE photos (
 CREATE TABLE albums (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(250) NOT NULL,
-    PRIMARY KEY(id)
+    user_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE photos_albums (
--- validation/user_validation.js
+++ validation/user_validation.js
@@ -1,25 +1,35 @@
 /**
- * Example Validation Rules
+ * User Validation Rules
  */
 
 const { body } = require('express-validator');
 const models = require('../models');
 
-/**
- * Create Example validation rules
- *
- * Required: title
- * Optional: -
- */
-const createRules = [body('title').exists().isLength({ min: 4 })];
+const createRules = [
+	body('email')
+		.exists()
+		.isLength({ min: 4 })
+		.custom(async value => {
+			const user = await new models.user_model({ email: value }).fetch({
+				require: false,
+			});
+			if (user) {
+				return Promise.reject('Email already exists.');
+			}
 
-/**
- * Update Example validation rules
- *
- * Required: -
- * Optional: title
- */
-const updateRules = [body('title').optional().isLength({ min: 4 })];
+			return Promise.resolve();
+		}),
+
+	body('password').exists().isLength({ min: 4 }),
+	body('first_name').exists().isLength({ min: 2 }),
+	body('last_name').exists().isLength({ min: 2 }),
+];
+
+const updateRules = [
+	body('password').optional().isLength({ min: 4 }),
+	body('first_name').optional().isLength({ min: 2 }),
+	body('last_name').optional().isLength({ min: 2 }),
+];
 
 module.exports = {
 	createRules,
