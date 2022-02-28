3d9fbea9fa558d9c77d9a53608d5f812b7caf1a7
Raymond Lam <raymond.m.lam@gmail.com>
2022-02-28 17:47:30 +0100

    added validation rules

---
+++ validation/album_validation.js
@@ -0,0 +1,46 @@
+/**
+ * Profile Validation Rules
+ */
+
+const { body } = require('express-validator');
+const models = require('../models');
+
+const createRules = [
+	body('title').exists().isLength({ min: 4 }),
+	body('user_id')
+		.exists()
+		.bail()
+		.custom(async value => {
+			const author = await new models.user_model({ id: value }).fetch({
+				require: false,
+			});
+			if (!author) {
+				return Promise.reject(`User with ID ${value} does not exist.`);
+			}
+
+			return Promise.resolve();
+		}),
+];
+
+// Tillåt endast lösen, för- och efternamn uppdateras.
+const updateRules = [
+	body('title').optional().isLength({ min: 4 }),
+	body('user_id')
+		.exists()
+		.bail()
+		.custom(async value => {
+			const author = await new models.user_model({ id: value }).fetch({
+				require: false,
+			});
+			if (!author) {
+				return Promise.reject(`User with ID ${value} does not exist.`);
+			}
+
+			return Promise.resolve();
+		}),
+];
+
+module.exports = {
+	createRules,
+	updateRules,
+};
