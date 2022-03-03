3494f6fbbd5d0667acdc1b14e5fedc3d252043bb
Raymond Lam <raymond.m.lam@gmail.com>
2022-03-03 10:48:07 +0100

    removed profile and moved rules to user

--- validation/user_validation.js
+++ validation/user_validation.js
@@ -26,12 +26,47 @@ const createRules = [
 ];
 
 const updateRules = [
+	body('email').optional().isLength({ min: 4 }),
 	body('password').optional().isLength({ min: 4 }),
 	body('first_name').optional().isLength({ min: 2 }),
 	body('last_name').optional().isLength({ min: 2 }),
 ];
 
+const addPhotoRules = [
+	body('photo_id')
+		.exists()
+		.bail()
+		.custom(async value => {
+			const book = await new models.photo_model({ id: value }).fetch({
+				require: false,
+			});
+			if (!book) {
+				return Promise.reject(`Photo with ID ${value} does not exist.`);
+			}
+
+			return Promise.resolve();
+		}),
+];
+
+const addAlbumRules = [
+	body('album_id')
+		.exists()
+		.bail()
+		.custom(async value => {
+			const book = await new models.album_model({ id: value }).fetch({
+				require: false,
+			});
+			if (!book) {
+				return Promise.reject(`Photo with ID ${value} does not exist.`);
+			}
+
+			return Promise.resolve();
+		}),
+];
+
 module.exports = {
 	createRules,
 	updateRules,
+	addPhotoRules,
+	addAlbumRules,
 };
