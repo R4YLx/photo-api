72366ef02da307167a1902d46c6c5d19b278c94d
Raymond Lam <raymond.m.lam@gmail.com>
2022-03-03 21:13:33 +0100

    restructred sql

--- photos.sql
+++ photos.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(250) NOT NULL,
-    last_name VARCHAR(250) NOT NULL,
     email VARCHAR(250) NOT NULL,
     password VARCHAR(250) NOT NULL,
+    first_name VARCHAR(250) NOT NULL,
+    last_name VARCHAR(250) NOT NULL,
     PRIMARY KEY(id)
 );
 
@@ -25,13 +25,9 @@ CREATE TABLE albums (
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-CREATE TABLE photos_albums (
+CREATE TABLE album_photos (
     id INT NOT NULL AUTO_INCREMENT,
-    photo_id INT NOT NULL,
-    album_id INT NOT NULL,
+    photo_id INTEGER UNSIGNED REFERENCES photos(id),
+    album_id INTEGER UNSIGNED REFERENCES albums(id),
     PRIMARY KEY (id)
-)
-
-ALTER TABLE photos_albums
-ADD CONSTRAINT FOREIGN KEY (`photo_id`) REFERENCES `users` (`id`),
-ADD CONSTRAINT FOREIGN KEY (`album_id`) REFERENCES `users` (`id`)
+);
