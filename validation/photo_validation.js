e8e7546a2a12342e813ac3a8360255338798dda0
Raymond Lam <raymond.m.lam@gmail.com>
2022-03-04 17:16:37 +0100

    Added rules for photo with URL checker

--- validation/photo_validation.js
+++ validation/photo_validation.js
@@ -6,13 +6,13 @@ const { body } = require('express-validator');
 
 const createRules = [
 	body('title').exists().isLength({ min: 4 }),
-	body('url').exists().isLength({ min: 4 }),
+	body('url').exists().isURL().isLength({ min: 4 }),
 	body('comment').optional().isLength({ min: 1 }),
 ];
 
 const updateRules = [
 	body('title').optional().isLength({ min: 4 }),
-	body('url').optional().isLength({ min: 4 }),
+	body('url').optional().isURL().isLength({ min: 4 }),
 	body('comment').optional().isLength({ min: 1 }),
 ];
 
