e2b1150526ab69648fa5b8fb64f72e08add9cadc
Raymond Lam <raymond.m.lam@gmail.com>
2022-03-05 10:56:37 +0100

    added rule for attaching photo to album

--- validation/album_validation.js
+++ validation/album_validation.js
@@ -8,7 +8,24 @@ const createRules = [body('title').exists().isLength({ min: 4 })];
 
 const updateRules = [body('title').optional().isLength({ min: 4 })];
 
+addPhotoToAlbumRules = [
+	body('photo_id')
+		.exists()
+		.isInt()
+		.bail()
+		.custom(async value => {
+			const photo = await new models.Photo({ id: value }).fetch({
+				require: false,
+			});
+			if (!photo) {
+				return Promise.reject(`Photo with ID ${value} does not exist.`);
+			}
+			return Promise.resolve();
+		}),
+];
+
 module.exports = {
 	createRules,
 	updateRules,
+	addPhotoToAlbumRules,
 };
