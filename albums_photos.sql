12acaf8e6a60211a63c21dbc11fd364b494a823d
Raymond Lam <raymond.m.lam@gmail.com>
2022-03-09 14:37:22 +0100

    update sql dump

--- albums_photos.sql
+++ albums_photos.sql
@@ -1,68 +1,135 @@
--- phpMyAdmin SQL Dump
--- version 5.1.2
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
--- Host: localhost
--- Generation Time: Mar 05, 2022 at 04:37 PM
--- Server version: 5.7.24
--- PHP Version: 7.4.16
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: photos
+-- ------------------------------------------------------
+-- Server version	5.7.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `photos`
+-- Table structure for table `albums`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `albums`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `albums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(250) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `albums_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `albums`
+--
+
+LOCK TABLES `albums` WRITE;
+/*!40000 ALTER TABLE `albums` DISABLE KEYS */;
+INSERT INTO `albums` VALUES (4,'Confetti Album',1),(5,'Changed The Name Of This Album',1),(6,'Confetti\'R\'Us',1),(7,'New Album',1),(8,'Ducks, duck, duck, goose',2),(9,'Throwback',2),(10,'Changing The Name Of This Album',2);
+/*!40000 ALTER TABLE `albums` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `albums_photos`
 --
 
+DROP TABLE IF EXISTS `albums_photos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `albums_photos` (
-  `id` int(11) NOT NULL,
-  `photo_id` int(10) UNSIGNED DEFAULT NULL,
-  `album_id` int(10) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `photo_id` int(10) unsigned DEFAULT NULL,
+  `album_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `albums_photos`
 --
 
-INSERT INTO `albums_photos` (`id`, `photo_id`, `album_id`) VALUES
-(1, 1, 4),
-(3, 8, 8),
-(4, 7, 9);
+LOCK TABLES `albums_photos` WRITE;
+/*!40000 ALTER TABLE `albums_photos` DISABLE KEYS */;
+INSERT INTO `albums_photos` VALUES (1,1,4),(3,8,8),(4,7,9),(5,9,11);
+/*!40000 ALTER TABLE `albums_photos` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Indexes for dumped tables
+-- Table structure for table `photos`
 --
 
---
--- Indexes for table `albums_photos`
---
-ALTER TABLE `albums_photos`
-  ADD PRIMARY KEY (`id`);
+DROP TABLE IF EXISTS `photos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `photos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(250) NOT NULL,
+  `url` varchar(250) NOT NULL,
+  `comment` varchar(250) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Dumping data for table `photos`
 --
 
---
--- AUTO_INCREMENT for table `albums_photos`
---
-ALTER TABLE `albums_photos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
+LOCK TABLES `photos` WRITE;
+/*!40000 ALTER TABLE `photos` DISABLE KEYS */;
+INSERT INTO `photos` VALUES (1,'Confetti Photo #1','https://images.unsplash.com/photo-1492684223066-81342ee5ff30','Confetti #1',1),(2,'Photo of confetti','https://images.unsplash.com/photo-1481162854517-d9e353af153d','Changed the name of this picture. Super fun!',1),(3,'Confetti Photo #3','https://images.unsplash.com/photo-1514525253161-7a46d19cd819','Confetti #3',1),(4,'New Photo','https://images.unsplash.com/new-photo-to-add','Pretty cool picture of a cat.',1),(5,'Ducky #1','https://images.ducks.com/photo-1','Duck on bridge',2),(6,'Photo of confetti','https://images.ducks.com/photo-2','Super fun!',2),(7,'Ducky #3','https://images.ducks.com/photo-3','Duck on car',2),(8,'Goose #1','https://images.goose.com/goose-1','The one and only',2);
+/*!40000 ALTER TABLE `photos` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `first_name` varchar(250) NOT NULL,
+  `last_name` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'user1@email.com','$2b$10$0YOcuJ5zakNSxeGX2y3g0OuRaOVnFkF/ywXtJtCDdClPgmqUpC1t2','Jane','Doe'),(2,'user2@email.com','$2b$10$p0H73AlbzGn83lrHJ5TSEuZjfADWpknZL2Sni5NaBoiOWLZX6yyrq','John','Doe'),(3,'testuser@email.com','$2b$10$yJZ/AslEkFoDG78rHzYja.hO./jlE2la88zJVGI2cgF6kTcC34uAS','Test','Testsson');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-09 14:32:57
